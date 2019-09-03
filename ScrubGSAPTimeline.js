fe7b45f57708b7d3e305a63fa589420b85fcb362
Chris Gannon <chris@gannon.tv>
2019-09-03 17:02:39 +0100

    Update ScrubGSAPTimeline.js

--- ScrubGSAPTimeline.js
+++ ScrubGSAPTimeline.js
@@ -38,7 +38,7 @@ window.ScrubGSAPTimeline = (tl) => {
             },
             box = document.createElement('div'),
             mouseX, mouseY, gGreen = '#53A018',
-            cssText = "position:fixed;border-radius:4px;font-size:14px;padding:5px;user-select:none;pointer-evenmts:none;text-align:center;color:#53A018;top:0;left:0;font-family:Helvetica, Arial, sans-serif;background-color:#262626;"
+            cssText = "position:fixed;border-radius:4px;font-size:14px;padding:5px;user-select:none;pointer-events:none;text-align:center;color:#53A018;top:0;left:0;font-family:Helvetica, Arial, sans-serif;background-color:#262626;"
         box.style.cssText = cssText;
 
         document.body.appendChild(box);
