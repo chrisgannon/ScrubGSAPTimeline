37556e452504f9825443c2063722b530fe845d9e
Chris Gannon <chris@gannon.tv>
2019-09-03 16:45:10 +0100

    Converted to ES6
    Fixed: time display position would be offset using some 3rd party CSS. Now sticks to mouse.
    Fixed: Border radius stopped working in new GSAP 3.0

--- ScrubGSAPTimeline.js
+++ ScrubGSAPTimeline.js
@@ -1,86 +1,76 @@
-window.ScrubGSAPTimeline = function(tl) {
-  var mainTl = new TimelineLite();
-if( Object.prototype.toString.call( tl ) === '[object Array]' ) {
-    var i = tl.length;
-    while(--i > -1){
-     mainTl.add(tl[i], 0);
+window.ScrubGSAPTimeline = (tl) => {
+    let mainTl = new TimelineLite();
+    if (Object.prototype.toString.call(tl) === '[object Array]') {
+        let i = tl.length;
+        while (--i > -1) {
+            mainTl.add(tl[i], 0);
+        }
+
+    } else {
+        mainTl = tl;
     }
-    
-} else {
- mainTl = tl;
-}
- var Scrubber = function(_tl){
-  
-   var mouseMove = function(e) {
-     box.innerHTML = parseFloat(_tl.time()).toFixed(2);
-     _tl.pause();
-     _tl.progress(e.pageX / (window.innerWidth)); // * tl.duration();
-     mouseX = e.pageX, mouseY = e.pageY;
+    const Scrubber = (_tl) => {
 
-     TweenLite.set(box, {
-      x: (mouseX >= (window.innerWidth - 48)) ? mouseX - 48 : mouseX,
-      y: (mouseY <= 20) ? mouseY + 20 : mouseY - 20
-     })
+        let mouseMove = (e) => {
+                box.innerHTML = parseFloat(_tl.time()).toFixed(2);
+                _tl.pause();
+                _tl.progress(e.clientX / (window.innerWidth)); // * tl.duration();
+                mouseX = e.clientX, mouseY = e.clientY;
 
-  },
-   mouseOver = function(e) {
-     _tl.pause();
-     _tl.play(0);
-     TweenLite.set(box, {
-      autoAlpha: 1
-     })
-  },
-   mouseOut = function(e) {
-     _tl.play(e.pageX / (window.innerWidth) * _tl.duration());
-     TweenLite.set(box, {
-      autoAlpha: 0
-     })
-  },
-  box = document.createElement('div'),
-  mouseX, mouseY, gGreen = '#53A018';
-  TweenLite.set(box, {
-    position: 'absolute',
-    borderRadius: 4,
-    fontSize: 14,
-    padding: '5px 5px 5px 5px',
-    userSelect: 'none',
-    pointerEvents:'none',
-    textAlign: 'center',
-    color: gGreen,
-    fontFamily: 'Helvetica, Arial, sans-serif',
-    backgroundColor: '#262626'
-   });
+                TweenLite.set(box, {
+                    x: (mouseX >= (window.innerWidth - 48)) ? mouseX - 48 : mouseX,
+                    y: (mouseY <= 20) ? mouseY + 20 : mouseY - 20
+                })
+                console.log(mouseX)
+            },
+            mouseOver = (e) => {
+                _tl.pause();
+                _tl.play(0);
+                TweenLite.set(box, {
+                    autoAlpha: 1
+                })
+            },
+            mouseOut = (e) => {
+                _tl.play(e.clientX / (window.innerWidth) * _tl.duration());
+                TweenLite.set(box, {
+                    autoAlpha: 0
+                })
+            },
+            box = document.createElement('div'),
+            mouseX, mouseY, gGreen = '#53A018',
+            cssText = "position:fixed;border-radius:4px;font-size:14px;padding:5px;user-select:none;pointer-evenmts:none;text-align:center;color:#53A018;top:0;left:0;font-family:Helvetica, Arial, sans-serif;background-color:#262626;"
+        box.style.cssText = cssText;
 
- document.body.appendChild(box);
- document.body.onmousemove = mouseMove;
- document.body.onmouseover = mouseOver;
- document.body.onmouseout = mouseOut;
- document.body.ondblclick = function(e) {
-  if (!document.body.onmousemove) {
-   TweenLite.to(box, 0.2, {
-    color: gGreen
-   })
-   document.body.onmousemove = mouseMove;
-   document.body.onmouseover = mouseOver;
-   document.body.onmouseout = mouseOut;
-   return;
-  }
+        document.body.appendChild(box);
+        document.body.onmousemove = mouseMove;
+        document.body.onmouseover = mouseOver;
+        document.body.onmouseout = mouseOut;
+        document.body.ondblclick = (e) => {
+            if (!document.body.onmousemove) {
+                TweenLite.to(box, 0.2, {
+                    color: gGreen
+                })
+                document.body.onmousemove = mouseMove;
+                document.body.onmouseover = mouseOver;
+                document.body.onmouseout = mouseOut;
+                return;
+            }
 
-  TweenLite.to(box, 0.2, {
-   color: '#A31632'
-  })
-  document.body.onmousemove = null;
-  document.body.onmouseover = null;
-  document.body.onmouseout = null;
-  _tl.pause();
+            TweenLite.to(box, 0.2, {
+                color: '#A31632'
+            })
+            document.body.onmousemove = null;
+            document.body.onmouseover = null;
+            document.body.onmouseout = null;
+            _tl.pause();
 
- }   
- 
-       
- }
+        }
 
- var s = new Scrubber(mainTl);
-  
- 
+
+    }
+
+    Scrubber(mainTl);
+
+
 
 }
